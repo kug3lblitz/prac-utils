ed65dbfe30899da047f0315e2ca4407eab9a65ee
Patrick Werner <kug3lblitz@icloud.com>
2024-03-28 09:45:31 -0400

    further improvements to target specific files

--- bulk_rename/rename.py
+++ bulk_rename/rename.py
@@ -2,12 +2,16 @@ import os
 import re
 import argparse
 
-def replace_text_in_files(target, old_text, new_text, file_extension=None, is_file=False):
+# example usage
+# python rename.py -d inetpub/ www.site1.com www.site2.com .config
+# python rename.py -d inetpub/ www.site1.com www.site2.com -n Web.config
+
+def replace_text_in_files(target, old_text, new_text, file_extension=None, is_file=False, specific_filename=None):
     encodings = ['utf-8', 'latin1', 'cp1252']  # Add more if needed
     if is_file:
         files = [target]
     else:
-        files = [os.path.join(foldername, filename) for foldername, _, filenames in os.walk(target) for filename in filenames if not file_extension or filename.endswith(file_extension)]
+        files = [os.path.join(foldername, filename) for foldername, _, filenames in os.walk(target) for filename in filenames if (not file_extension or filename.endswith(file_extension)) and (not specific_filename or filename == specific_filename)]
     
     for filepath in files:
         for encoding in encodings:
@@ -31,10 +35,11 @@ if __name__ == "__main__":
     parser.add_argument('old_text', help='The text to replace')
     parser.add_argument('new_text', help='The text to replace with')
     parser.add_argument('file_extension', nargs='?', default=None, help='The file extension to target (optional)')
+    parser.add_argument('-n', '--name', help='The specific filename to target (optional)')
     
     args = parser.parse_args()
     
     if args.file:
-        replace_text_in_files(args.file, args.old_text, args.new_text, args.file_extension, is_file=True)
+        replace_text_in_files(args.file, args.old_text, args.new_text, args.file_extension, is_file=True, specific_filename=args.name)
     else:
-        replace_text_in_files(args.directory, args.old_text, args.new_text, args.file_extension, is_file=False)
+        replace_text_in_files(args.directory, args.old_text, args.new_text, args.file_extension, is_file=False, specific_filename=args.name)

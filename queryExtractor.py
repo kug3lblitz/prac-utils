e8e411e1b7d2509b7b466a881681dd806c4f144b
Patrick Werner <kug3lblitz@icloud.com>
2024-04-10 09:53:04 -0400

    more improvements

--- queryExtractor.py
+++ queryExtractor.py
@@ -12,11 +12,19 @@ with open(output_file, 'w') as f:
         for file in files:
             if file.endswith('.cs'):
                 with open(os.path.join(root, file), 'r') as code_file:
-                    for line in code_file:
+                    lines = code_file.readlines()
+                    for i in range(len(lines)):
+                        line = lines[i]
                         function_match = function_pattern.search(line)
                         if function_match:
                             current_function = function_match.group(2)
-                        if any(keyword in line.upper() for keyword in sql_keywords):
+                        if any(keyword in line for keyword in sql_keywords):
                             f.write(f'File: {os.path.join(root, file)}\n')
                             f.write(f'Function: {current_function}\n')
+                            f.write(f'Line number: {i + 1}\n')
                             f.write(f'Query: {line}\n')
+                            if i + 1 < len(lines):
+                                f.write(f'Next line: {lines[i + 1]}\n')
+                            if i + 2 < len(lines):
+                                f.write(f'Next next line: {lines[i + 2]}\n')
+                            f.write('-----------------------------------------\n')
